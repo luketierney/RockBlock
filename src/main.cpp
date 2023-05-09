a53f2811ca6837d2bc6658f14284d584b61d1751
Luke <luketierney06@gmail.com>
2023-05-09 13:02:42 -0400

    Added variables for ports

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,12 @@
 #include <Arduino.h>
+int ReadyRed = 13;
+int ReadyRed = 12;
+int NetReadyRed = 11;
+int NetReadyGreen = 10;
+int MsgStateRed = 9;
+int MsgStateRed = 8;
+int MsgStateRed = 7;
+
 String AT(String x){
   Serial.println(x + "\r");
   delay(100);
@@ -9,17 +17,19 @@ void setup() {
   for(int x = 7; x <= 13; x += 1){
     pinMode(x, OUTPUT);
   }
-  
+
   Serial.begin(19200);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   digitalWrite(2, HIGH);
-  if(AT("AT") == "OK"){
+  /*if(AT("AT") == "OK"){
     Serial.println("Luke OK");
   }
   else{
     Serial.println(AT("AT"));
   }
+  */
+
 }
