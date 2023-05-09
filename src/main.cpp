0f4f0765379b8eab1bb931bcdacc3bea1328d77f
Luke <luketierney06@gmail.com>
2023-05-09 13:03:38 -0400

    Fixed variable names

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,11 @@
 #include <Arduino.h>
 int ReadyRed = 13;
-int ReadyRed = 12;
+int ReadyGreen = 12;
 int NetReadyRed = 11;
 int NetReadyGreen = 10;
 int MsgStateRed = 9;
-int MsgStateRed = 8;
-int MsgStateRed = 7;
+int MsgStateYellow = 8;
+int MsgStateGreen = 7;
 
 String AT(String x){
   Serial.println(x + "\r");
