1dae156597df7ff5d5cd9b668bb4198f0f8d1b1a
Luke <luketierney06@gmail.com>
2023-05-10 12:29:54 -0400

    Update

--- src/main.cpp
+++ src/main.cpp
@@ -7,28 +7,28 @@ int MsgStateR = 9;
 int MsgStateY = 8;
 int MsgStateG = 7;
 int SleepWake = 6;
-int TXD = 5;
+int TXD = 0;
 int NetAV = 4;
 int RTS = 3;
 int CTS = 2;
 int RXD = 1;
 String AT(String x){
-  Serial.println(x + "\r");
-  delay(100);
+  Serial.println(x + '\r');
+  delay(10000);
   return String(Serial.read());
 }
 void setup() {
   // put your setup code here, to run once:
-  int out[] = {5,3,6,7,8,9,10,11,12,13};
-  int in[] = {4,2,1};
-  for(int outpin: out){
+  int out[] = {TXD,RTS,SleepWake,MsgStateG,MsgStateY,MsgStateR,NetReadyG,NetReadyR,ReadyR,ReadyG};
+  int in[] = {NetAV,CTS,RXD};
+  for(int outpin : out){
     pinMode(outpin, OUTPUT);
   }
   for(int inpin : in){
     pinMode(inpin, INPUT);
   }
   Serial.begin(19200);
-  Serial1.begin(9600);
+  Serial.println("TEST");
 }
 
 void loop() {
@@ -36,15 +36,15 @@ void loop() {
   digitalWrite(SleepWake, HIGH);
   delay(15000);
   if(AT("AT") == "OK"){
-    Serial1.println("Luke OK");
+    Serial.println("Luke OK");
     digitalWrite(ReadyG, HIGH);
     digitalWrite(ReadyR, LOW);
   }
   else{
     digitalWrite(ReadyG, LOW);
     digitalWrite(ReadyR, HIGH);
-    Serial1.println("Not OK");
-    Serial1.println(AT("AT"));
+    Serial.println("Not OK");
+    Serial.println("LUKE" + AT("AT"));
   }
   
 
