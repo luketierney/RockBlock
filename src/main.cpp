9b46a100546c853db8c5c876a7105222734c7470
Luke <luketierney06@gmail.com>
2023-05-11 13:10:01 -0400

    Update

--- src/main.cpp
+++ src/main.cpp
@@ -10,21 +10,24 @@ int NetReadyG = 10;
 int MsgStateR = 9;
 int MsgStateY = 8;
 int MsgStateG = 7;
-int SleepWake = 6;
-int TXD = 0;
+//int SleepWake = 6;
+int TXD = 18;
 int NetAV = 4;
 int RTS = 3;
 int CTS = 2;
-int RXD = 1;
+int RXD = 19;
 String AT(String x){
   Serial.println(x + '\r');
   Serial.println(Serial.read());
   delay(10000);
   return String(Serial.read());
 }
+uint8_t buffer[200] = 
+{ 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89 };
 void setup() {
   // put your setup code here, to run once:
-  int out[] = {TXD,RTS,SleepWake,MsgStateG,MsgStateY,MsgStateR,NetReadyG,NetReadyR,ReadyR,ReadyG};
+  int signalQuality = -1;
+  int out[] = {TXD,RTS,MsgStateG,MsgStateY,MsgStateR,NetReadyG,NetReadyR,ReadyR,ReadyG};
   int in[] = {NetAV,CTS,RXD};
   for(int outpin : out){
     pinMode(outpin, OUTPUT);
@@ -33,27 +36,76 @@ void setup() {
     pinMode(inpin, INPUT);
   }
   Serial.begin(115200);
+  while (!Serial);
   IriduimSerial.begin(19200);
-  Serial.println("TEST");
   modem.setPowerProfile(IridiumSBD::DEFAULT_POWER_PROFILE);
   if (modem.begin() != ISBD_SUCCESS){
     Serial.println("Couldn't begin modem operations.");
+    exit(0);
   }
+
+  int err = modem.getSignalQuality(signalQuality);
+  if (err != 0)
+  {
+    Serial.print("SignalQuality failed: error ");
+    Serial.println(err);
+    exit(1);
+  }
+  Serial.print("Signal quality is ");
+  Serial.println(signalQuality);
+
 }
+//static bool messageSent = false;
 
 void loop() {
   // put your main code here, to run repeatedly:
-  if(AT("AT") == "OK"){
-    IriduimSerial.println("Luke OK");
-    digitalWrite(ReadyG, HIGH);
-    digitalWrite(ReadyR, LOW);
-  }
-  else{
-    digitalWrite(ReadyG, LOW);
-    digitalWrite(ReadyR, HIGH);
-    IriduimSerial.println("Not OK");
-    Serial.println("LUKE" + AT("AT"));
-  }
-  
+  /*int err;
+  if (!messageSent || modem.getWaitingMessageCount() > 0)
+  {
+    size_t bufferSize = sizeof(buffer);
 
+    // First time through send+receive; subsequent loops receive only
+    if (!messageSent)
+      err = modem.sendReceiveSBDBinary(buffer, 11, buffer, bufferSize);
+    else
+      err = modem.sendReceiveSBDText(NULL, buffer, bufferSize);
+      
+    if (err != ISBD_SUCCESS)
+    {
+      Serial.print("sendReceiveSBD* failed: error ");
+      Serial.println(err);
+    }
+    else // success!
+    {
+      messageSent = true;
+      Serial.print("Inbound buffer size is ");
+      Serial.println(bufferSize);
+      for (int i=0; i<bufferSize; ++i)
+      {
+        Serial.print(buffer[i], HEX);
+        if (isprint(buffer[i]))
+        {
+          Serial.print("(");
+          Serial.write(buffer[i]);
+          Serial.print(")");
+        }
+        Serial.print(" ");
+      }
+      Serial.println();
+      Serial.print("Messages remaining to be retrieved: ");
+      Serial.println(modem.getWaitingMessageCount());
+    }
+  }
+*/
 }
+#if DIAGNOSTICS
+void ISBDConsoleCallback(IridiumSBD *device, char c)
+{
+  Serial.write(c);
+}
+
+void ISBDDiagsCallback(IridiumSBD *device, char c)
+{
+  Serial.write(c);
+}
+#endif
